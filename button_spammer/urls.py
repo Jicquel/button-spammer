a12c9a5087781f33e90eaf242913bd706e7b0b1a
Jicquel <vincent.jicquel@gmail.com>
2024-02-18 14:26:55 +0100

    Improve actions processing
    Add templates for action listing, deletion and creation, as well as for
    occurences creation. Improve the URLs accordingly.

--- button_spammer/urls.py
+++ button_spammer/urls.py
@@ -4,7 +4,10 @@ from . import views
 
 urlpatterns = [
     path("", views.index, name="index"),
-    path("register/<str:action_name>/", views.register),
+    path("create_occurence/<str:action_name>/", views.create_occurence, name="create_occurence"),
     path("create_action/", views.create_action, name="create_action"),
-    path("create_action_form/", views.create_action_form, name="create_action_form")
+    path("create_action_form/", views.create_action_form, name="create_action_form"),
+    path("action/<str:action_name>/", views.action, name="action"),
+    path("actions/", views.actions, name="actions"),
+    path("delete_action/<str:action_name>/", views.delete_action, name="delete_action"),
 ]
--- button_spammer/views.py
+++ button_spammer/views.py
@@ -2,24 +2,35 @@ from django.shortcuts import render
 from django.http import HttpResponse
 from django.template import loader
 
+
 from .models import Action, Occurence
+from django.template import loader
 
 # Create your views here.
 
 def index(request):
-    return HttpResponse("Hello, world. You're at the button-spammer index.")
+    template = loader.get_template("index.html")
+    return HttpResponse(template.render({}, request))
 
-def register(request, action_name):
-    action = Action.objects.filter(action_name=action_name)
-    if not action:
+##############
+# Occurences #
+##############
+
+def create_occurence(request, action_name):
+    act = Action.objects.filter(action_name=action_name)
+    if not act:
         return HttpResponse ("The action " + action_name + " does not exist. Can not create occurence.")
 
-    occ = Occurence (action_name=action.get())
+    occ = Occurence (action_name=act.get())
     occ.save()
-    return HttpResponse("Occurence for action " + action_name + " registered.")
+    return action(request, action_name)
+
+###########
+# Actions #
+###########
 
 def create_action_form(request):
-    template = loader.get_template("action_creation.html")
+    template = loader.get_template("actions/create.html")
     return HttpResponse(template.render({}, request))
 
 def create_action(request):
@@ -32,3 +43,35 @@ def create_action(request):
     action = Action (action_name = action_name)
     action.save()
     return HttpResponse("Action " + action_name + " has been created.")
+
+def action(request, action_name):
+    action = Action.objects.filter(action_name=action_name)
+
+    if not action:
+        return HttpResponse ("The action " + action_name + " does not exist.")
+
+    occurences=Occurence.objects.filter(action_name=action_name)
+
+    template = loader.get_template("actions/action.html")
+    context = {
+        "action": action.first,
+        "occurences_list" : occurences
+    }
+    return HttpResponse(template.render(context, request))
+
+def actions(request):
+    actions = Action.objects.all()
+    template = loader.get_template("actions/actions.html")
+    context = {
+        "actions_list": actions,
+    }
+    return HttpResponse(template.render(context, request))
+
+def delete_action(request, action_name):
+    action = Action.objects.filter(action_name=action_name)
+
+    if not action:
+        return HttpResponse ("The action " + action_name + " does not exist.")
+
+    action.delete()
+    return actions(request)
