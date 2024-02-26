785910c7a55c8e6a6cde105cb3fcbdbf681a4209
Jicquel <vincent.jicquel@gmail.com>
2024-02-26 18:59:38 +0100

    Return JSON or Web format for actions
    External application like the raspberry pi one need actions name.
    Add a JSON format response for these type of usage.

--- button_spammer/views.py
+++ button_spammer/views.py
@@ -1,5 +1,5 @@
 from django.shortcuts import render
-from django.http import HttpResponse
+from django.http import HttpResponse, JsonResponse
 from django.template import loader
 
 
@@ -8,30 +8,38 @@ from django.template import loader
 
 # Create your views here.
 
+
 def index(request):
     template = loader.get_template("index.html")
     return HttpResponse(template.render({}, request))
+
 
 ##############
 # Occurences #
 ##############
 
+
 def create_occurence(request, action_name):
     act = Action.objects.filter(action_name=action_name)
     if not act:
-        return HttpResponse ("The action " + action_name + " does not exist. Can not create occurence.")
+        return HttpResponse(
+            "The action " + action_name + " does not exist. Can not create occurence."
+        )
 
-    occ = Occurence (action_name=act.get())
+    occ = Occurence(action_name=act.get())
     occ.save()
     return action(request, action_name)
+
 
 ###########
 # Actions #
 ###########
 
+
 def create_action_form(request):
     template = loader.get_template("actions/create.html")
     return HttpResponse(template.render({}, request))
+
 
 def create_action(request):
     action_name = request.POST["name"]
@@ -40,38 +48,45 @@ def create_action(request):
     if action:
         return HttpResponse("Action " + action_name + " already exist.")
 
-    action = Action (action_name = action_name)
+    action = Action(action_name=action_name)
     action.save()
     return HttpResponse("Action " + action_name + " has been created.")
+
 
 def action(request, action_name):
     action = Action.objects.filter(action_name=action_name)
 
     if not action:
-        return HttpResponse ("The action " + action_name + " does not exist.")
+        return HttpResponse("The action " + action_name + " does not exist.")
 
-    occurences=Occurence.objects.filter(action_name=action_name)
+    occurences = Occurence.objects.filter(action_name=action_name)
 
     template = loader.get_template("actions/action.html")
-    context = {
-        "action": action.first,
-        "occurences_list" : occurences
-    }
+    context = {"action": action.first, "occurences_list": occurences}
     return HttpResponse(template.render(context, request))
 
-def actions(request):
+
+def actions(request, format):
     actions = Action.objects.all()
+
     template = loader.get_template("actions/actions.html")
+
+    if format == "json":
+        data = {}
+        data["actions"] = [{"name": a.action_name} for a in actions]
+        return JsonResponse(data)
+
     context = {
         "actions_list": actions,
     }
     return HttpResponse(template.render(context, request))
 
+
 def delete_action(request, action_name):
     action = Action.objects.filter(action_name=action_name)
 
     if not action:
-        return HttpResponse ("The action " + action_name + " does not exist.")
+        return HttpResponse("The action " + action_name + " does not exist.")
 
     action.delete()
     return actions(request)
